baf4c47987c5d9fa3df220f9fc6e4408d734138e
HASHIM BOLAJI BELLO <bolaji1050@gmail.com>
2023-04-27 00:55:32 +0800

    instructor profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,156 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useInstructor } from "../context/InstructorContext";
+import {
+  BsCashCoin,
+  BsCashStack,
+  BsClockHistory,
+  BsPerson,
+  BsPersonCheck,
+  BsPersonX,
+} from "react-icons/bs";
+import moment from "moment";
+import pricingData from "../utils/pricing.json";
+import { useStudent } from "../context/StudentContext";
 
 const Profile = () => {
+  const { instructor } = useInstructor();
+  const { students, getStudents } = useStudent();
+  const [paidTime, setPaidTime] = useState(0);
+  const [payments, setPayments] = useState([]);
+
+  useEffect(() => {
+    if (instructor) getStudents(instructor.uid);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [instructor]);
+
+  const paymentData = students.map((stud) => {
+    if (stud.classes) {
+      return stud.classes;
+    } else return [];
+  });
+
+  const presentTotal = paymentData
+    .flat()
+    .filter((stud) => stud.attended === "present")
+    .reduce((acc, val) => acc + pricingData[val.plan], 0);
+
+  const absentTotal = paymentData
+    .flat()
+    .filter((stud) => stud.attended === "absent")
+    .reduce((acc, val) => acc + pricingData[val.plan], 0);
+
+  const total = absentTotal + presentTotal;
+
+  useEffect(() => {
+    if (instructor) {
+      setPaidTime(
+        instructor.payments?.reduce((acc, val) => acc + val.payment * 1, 0) || 0
+      );
+      setPayments(instructor.payments || []);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [instructor]);
+
   return (
-    <div className="p-5">
-      <p>Profile</p>
-    </div>
+    <>
+      {instructor ? (
+        <div className="space-y-8 px-5 py-12">
+          <div className=" grid grid-cols-6 gap-8">
+            <div className="col-span-6 lg:col-span-3 2xl:col-span-2 rounded-lg shadow-lg p-8 space-y-4">
+              <div className="flex items-center justify-between">
+                <p className="font-semibold text-xl">Profile</p>
+                <div className="rounded-full bg-blue-300 bg-opacity-40 p-3">
+                  <BsPerson className="text-blue-700 text-xl" />
+                </div>
+              </div>
+              <div className="text-center space-y-2">
+                <p className="text-xl font-semibold">{instructor?.fullName}</p>
+                <p className="text-gray-500 text-sm">{instructor?.email}</p>
+              </div>
+            </div>
+            <div className="col-span-6 lg:col-span-3 2xl:col-span-2 rounded-lg shadow-lg p-8 space-y-4">
+              <div className="flex items-center justify-between">
+                <p className="font-semibold text-xl">Total Duration</p>
+              </div>
+              <div className="space-y-2 text-center">
+                <p className="font-semibold text-xl">{total}</p>
+                <p className="text-gray-500 text-sm">Total Minutes</p>
+              </div>
+            </div>
+            <div className="col-span-3 2xl:col-span-1 rounded-lg shadow-lg px-5 py-8 space-y-4">
+              <div className="flex items-center justify-between">
+                <p className="font-semibold text-xl">Present</p>
+                <div className="rounded-full bg-purple-300 bg-opacity-40 p-3">
+                  <BsPersonCheck className="text-purple-700 text-xl" />
+                </div>
+              </div>
+              <div className="space-y-2 text-center">
+                <p className="font-semibold text-xl">{presentTotal}</p>
+                <p className="text-gray-500 text-sm"> Minutes</p>
+              </div>
+            </div>
+            <div className="col-span-3 2xl:col-span-1 rounded-lg shadow-lg px-5 py-8 space-y-4">
+              <div className="flex items-center justify-between">
+                <p className="font-semibold text-xl">No Show</p>
+                <div className="rounded-full bg-indigo-300 bg-opacity-40 p-3">
+                  <BsPersonX className="text-indigo-700 text-xl" />
+                </div>
+              </div>
+              <div className="space-y-2 text-center">
+                <p className="font-semibold text-xl">{absentTotal}</p>
+                <p className="text-gray-500 text-sm"> Minutes</p>
+              </div>
+            </div>
+            <div className="col-span-3 2xl:col-span-2 rounded-lg shadow-lg p-8 space-y-4">
+              <div className="flex items-center justify-between">
+                <p className="font-semibold text-xl">Paid Time</p>
+                <div className="rounded-full bg-red-300 bg-opacity-40 p-3">
+                  <BsCashStack className="text-red-700 text-xl" />
+                </div>
+              </div>
+              <div className="text-center space-y-2">
+                <p className="text-xl font-semibold">{paidTime}</p>
+                <p className="text-gray-500 text-sm">Minutes</p>
+              </div>
+            </div>
+            <div className="col-span-3 2xl:col-span-1 rounded-lg shadow-lg px-5 py-8 space-y-4">
+              <div className="flex items-center justify-between">
+                <p className="font-semibold text-xl">Unpaid</p>
+                <div className="rounded-full bg-slate-300 bg-opacity-40 p-3">
+                  <BsCashCoin className="text-slate-700 text-xl" />
+                </div>
+              </div>
+              <div className="space-y-2 text-center">
+                <p className="font-semibold text-xl">{total - paidTime}</p>
+                <p className="text-gray-500 text-sm"> Minutes</p>
+              </div>
+            </div>
+            <div className="col-span-6 2xl:col-span-3 max-h-48 rounded-lg shadow-lg p-8 space-y-4  overflow-y-auto">
+              <div className="flex items-center justify-between">
+                <p className="font-semibold text-xl">Payment History</p>
+                <div className="rounded-full bg-orange-300 bg-opacity-40 p-3">
+                  <BsClockHistory className="text-orange-700 text-xl" />
+                </div>
+              </div>
+              <div className="text-center">
+                {payments?.map((pay) => (
+                  <div
+                    key={pay.id}
+                    className="flex items-center justify-between text-sm text-gray-500 space-y-2"
+                  >
+                    <p>{moment(pay.payDay).format("MMMM Do YYYY")}</p>
+                    <p>{pay.payment} minutes</p>
+                  </div>
+                ))}
+              </div>
+              <div className="text-sm text-center text-gray-500">
+                {payments.length === 0 ? <p>No Payment History</p> : null}
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </>
   );
 };
 
